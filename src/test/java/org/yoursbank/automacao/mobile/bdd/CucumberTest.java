168d5b0e9b912f4ea65fdbaf3bce60b9b6442c7d
Kelvin Bellan Borges <80979622+KelvinBellanBorges@users.noreply.github.com>
2023-10-29 22:15:11 -0300

    Concluída a automação do processo de cadastro da T1, abrangendo tanto o cadastro do responsável quanto do dependente com sucesso

--- src/test/java/org/yoursbank/automacao/mobile/bdd/CucumberTest.java
+++ src/test/java/org/yoursbank/automacao/mobile/bdd/CucumberTest.java
@@ -5,13 +5,12 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("org/yoursbank/automacao/mobile/bdd")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.yoursbank.automacao.mobile.bdd")
-@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@SignUpRespCenario7")
+//@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@SignUpRespCOMPLETE")
 public class CucumberTest {
 }
