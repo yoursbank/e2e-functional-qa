0538e7a7f6e60e7eca24138c396131c4e85632e0
Amilton <amiltonjosefn2019@gmail.com>
2023-12-15 17:07:23 -0300

    refactor:Added exception handling and generic factory documentation

--- src/test/java/com/yoursbank/automacao/mobile/pages/MasterPageFactory.java
+++ src/test/java/com/yoursbank/automacao/mobile/pages/MasterPageFactory.java
@@ -1,24 +1,39 @@
 package com.yoursbank.automacao.mobile.pages;
 
 import com.yoursbank.automacao.mobile.Hooks;
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.support.PageFactory;
+
 @Slf4j
 public class MasterPageFactory {
-    public static <T> T getPage(Class<T> cls){
+    public static <T> T getPage(Class<T> cls) {
+        /**
+         * Cria uma instância da página especificada e inicializa os elementos usando AppiumFieldDecorator.
+         *
+         * @param cls a classe da página a ser instanciada
+         * @param <T> o tipo da página
+         * @return uma instância da página especificada
+         * @throws RuntimeException se houver um erro durante a instanciação
+         */
         T page;
 
         try {
-            page = cls.newInstance();
-            PageFactory.initElements(new AppiumFieldDecorator(Hooks.getDriver()), page);
+            if (cls.getDeclaredConstructor().getParameterCount() > 0) {
+                throw new RuntimeException("A classe deve ter um construtor padrão (sem argumentos).");
+            }
 
-        }catch (Exception e){
-            log.error("Error on page instantiation", e);
-            throw new RuntimeException(e);
+            page = cls.getDeclaredConstructor().newInstance();
+            AppiumDriver driver = Hooks.getDriver();
+            PageFactory.initElements(new AppiumFieldDecorator(driver), page);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Erro na instanciação da página", e);
         }
 
         return page;
     }
-
 }
+
+
